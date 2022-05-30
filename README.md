fda0dbde73c411021731f17701d419ff1e675778
Eoin <79023828+eoin-cr@users.noreply.github.com>
2022-05-30 20:33:10 +0100

    Added basic website

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # scheduler
 Program which schedules events, returning the date that suits the most people
 
-At the moment it's just a basic command line tool, but I plan to expand it to
-a website in the future.
+This is a rather basic website for now.  I plan to improve a number of things
+about it in the future, including being able to generate schedules accessible
+by different links, so different groups of people can use it simultaneously
--- main.py
+++ main.py
@@ -1,79 +1,94 @@
 from datetime import datetime
 import time
 import sys
+from flask import Flask, render_template, request
+from gevent.pywsgi import WSGIServer
 
-
-def take_input(name):
-    output = int(input(f"Enter the {name} in numerical form or enter -1 to stop: "))
-    if output == -1:
-        sys.exit()
-    return output
-
+app = Flask(__name__)
 
 start_times = []
 end_times = []
 
 
-def get_dates():
-    while True:
-        print("----------")
-        print("Start date")
-        day = take_input("day")
-        month = take_input("month")
-        year = take_input("year")
+def calculate(start_date, end_date):
+    output = []
+    # get_dates(start_date, end_date)
+    # start_str = start_date[:-3]+start_date[-2:]
+    start_date_unix = datetime.strptime(start_date, "%Y-%m-%d").timestamp()
+    # start_times.append(datetime.strptime(start_date, "%Y-%m-%d"))
+    start_times.append(start_date_unix)
+    print(start_date_unix)
+    print(f"start list: {start_times}")
+    end_date_unix = datetime.strptime(end_date, "%Y-%m-%d").timestamp()
+    print(end_date_unix)
+    # end_times.append(datetime.strptime(end_date, "%Y-%m-%d"))
+    end_times.append(end_date_unix)
+    print(f"end list: {end_times}")
 
-        date_time = datetime(year, month, day, 0, 0)
+    all_times = start_times + end_times
+    # print(all_times)
+    all_times.sort()
+    # print(all_times)
 
-        print(f"unix {time.mktime(date_time.timetuple())}")
-        start_times.append(time.mktime(date_time.timetuple()))
+    counter = 0
+    max_counter = 0
+    max_counter_date = None
 
-        print("----------")
-        print("End date")
+    for date in all_times:
+        if date in start_times and date in end_times:
+            # print(1)
+            continue
+        elif date in start_times:
+            # print(2)
+            counter += 1
+        else:
+            # print(3)
+            counter -= 1
 
-        day = take_input("day")
-        month = take_input("month")
-        year = take_input("year")
+        if counter > max_counter:
+            max_counter = counter
+            max_counter_date = date
 
-        date_time = datetime(year, month, day, 0, 0)
-        end_times.append(time.mktime(date_time.timetuple()))
+    print(f"counter: {max_counter}")
+    print(f"date: {max_counter_date}")
+    if max_counter <= 1:
+        # print("No times could be found that suited everyone!")
+        output.append("N/A")
+        output.append("N/A")
 
-        print("---------")
-        cont = int(input("Enter 1 to continue adding more dates or 0 to stop: "))
-        if cont == 0:
-            return
-
-
-get_dates()
-
-all_times = start_times + end_times
-print(all_times)
-all_times.sort()
-print(all_times)
-
-counter = 0
-max_counter = 0
-max_counter_date = None
-
-for date in all_times:
-    if date in start_times and date in end_times:
-        print(1)
-        continue
-    elif date in start_times:
-        print(2)
-        counter += 1
     else:
-        print(3)
-        counter -= 1
+        output.append(datetime.utcfromtimestamp(int(max_counter_date + 4000)).strftime('%Y-%m-%d'))
+        output.append(max_counter)
+        # print(f"The date that suits {max_counter} people is "
+        #       f"{datetime.utcfromtimestamp(int(max_counter_date)).strftime('%Y-%m-%d')}")
 
-    if counter > max_counter:
-        max_counter = counter
-        max_counter_date = date
+    return output
 
-print(max_counter)
-if max_counter <= 1:
-    print("No times could be found that suited everyone!")
 
-else:
-    print(f"The date that suits {max_counter} people is "
-          f"{datetime.utcfromtimestamp(int(max_counter_date)).strftime('%Y-%m-%d')}")
+@app.route('/')
+def index():
+    return render_template('index.html', date="N/A", amount="N/A")
 
+
+@app.route('/', methods=['POST'])
+def html_input():
+    start_date = request.form['start']
+    end_date = request.form['end']
+    # print(start_date)
+    # print(end_date)
+    calc_output = calculate(start_date, end_date)
+    optimal_date = calc_output[0]
+    best_for = calc_output[1]
+    # optimal_date = calculate()[0]
+    # best_for = calculate()[1]
+
+    return render_template('index.html', date=optimal_date, amount=best_for)
+
+
+if __name__ == '__main__':
+    # Debug/Development
+    # app.run(debug=True)
+
+    # Production
+    http_server = WSGIServer(('', 6565), app)
+    http_server.serve_forever()
